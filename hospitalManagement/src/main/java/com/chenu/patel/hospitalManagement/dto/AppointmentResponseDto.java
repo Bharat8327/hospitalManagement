b880daaf19aebdcc60a8695db718ec2cad65ad88
Bharat8327 <bp609213@gmail.com>
2026-06-08 10:54:22 +0530

    add appointmentDto

---
+++ hospitalManagement/src/main/java/com/chenu/patel/hospitalManagement/dto/AppointmentResponseDto.java
@@ -0,0 +1,18 @@
+package com.chenu.patel.hospitalManagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppointmentResponseDto {
+    private Long id;
+    private LocalDateTime appointmentTime;
+    private String reason;
+    private String patientName;
+//    private PatientResponseDto patient;
+}
