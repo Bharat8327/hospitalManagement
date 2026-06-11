22edad36884785bb2061c0e704c2af9e7a35f136
Bharat8327 <bp609213@gmail.com>
2026-06-11 10:38:07 +0530

    implement Oauth2 production grade (google , github, facebook , Twitter etc).

---
+++ hospitalManagement/src/main/java/com/chenu/patel/hospitalManagement/entity/type/AuthProviderType.java
@@ -0,0 +1,9 @@
+package com.chenu.patel.hospitalManagement.entity.type;
+
+public enum AuthProviderType {
+    GOOGLE,
+    FACEBOOK,
+    TWITTER,
+    GITHUB,
+    EMAIL
+}
---
+++ hospitalManagement/src/main/java/com/chenu/patel/hospitalManagement/entity/type/RoleType.java
@@ -0,0 +1,7 @@
+package com.chenu.patel.hospitalManagement.entity.type;
+
+public enum RoleType {
+    ADMIN,
+    PATIENT,
+    DOCTOR,
+}
